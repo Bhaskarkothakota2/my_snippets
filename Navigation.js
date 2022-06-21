b67f4b41ec50a2b4ae60ad9ce97b8346029aacad
Bhaskarkothakota2 <105773951+Bhaskarkothakota2@users.noreply.github.com>
2022-06-21 14:19:51 +0530

    Navigation to target area

---
+++ Navigation.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
+
+function Navigation() {
+  return (
+    <BrowserRouter>
+      <>
+        <div>
+          <Link to="#services">Services</Link>
+        </div>
+        <div>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+
+        </div>
+        <div>
+
+        <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+           <h1>sample</h1>
+            
+            <h4 id="services">hi this belongs to services </h4>
+            <h1>sample</h1>
+            <h1>sample</h1>
+            <h1>sample</h1>
+            <h1>sample</h1>
+            <h1>sample</h1>
+        </div>
+      </>
+    </BrowserRouter>
+  );
+}
+
+export default Navigation;
