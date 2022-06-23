e39be9f7730dc400b300a937952115b907fa1ea4
Bhaskarkothakota2 <105773951+Bhaskarkothakota2@users.noreply.github.com>
2022-06-23 11:39:59 +0530

    Table Pagination

---
+++ Pagination3.js
@@ -0,0 +1,68 @@
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function StickyHeadTable() {
+  const [tableData, setTableData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/albums")
+      .then((res) => setTableData(res.data));
+  }, []);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: "60%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow style={{color : 'black'}}>
+              <TableCell align="center">User ID</TableCell>
+              <TableCell align="center">ID</TableCell>
+              <TableCell align="center">Title</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tableData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return(
+                <TableRow>
+                  <TableCell align="center">{row.userId}</TableCell>
+                  <TableCell align="center">{row.id}</TableCell>
+                  <TableCell align="center">{row.title}</TableCell>
+                </TableRow>
+              )})}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 15]}
+        component="div"
+        count={tableData.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
